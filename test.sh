3eee9e5a28fe35aa5b6e93cd5936cb23b234490c
tifayuki <tifayuki@gmail.com>
2015-07-31 11:55:58 +0200

    fix test

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@
 echo "=> Building testing environment image"
 docker build -t test-haproxy -f Dockerfile-test .
 
-docker run --privileged -ti --rm -e DOCKER_USER=${DOCKER_USER} -e DOCKER_PASS=${DOCKER_PASS} -e TUTUM_USER=${TUTUM_USER} -e TUTUM_APIKEY=${TUTUM_APIKEY} test-haproxy
+docker run --privileged -ti --rm test-haproxy
