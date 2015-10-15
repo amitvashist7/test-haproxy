361d82b5842c4a2e02e2444ddfb0a362697ca89a
tifayuki <tifayuki@gmail.com>
2015-10-15 17:14:42 +0200

    manually reload haproxy on USR1 signal

--- haproxy/main.py
+++ haproxy/main.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import sys
+import signal
 
 import tutum
 
@@ -11,6 +12,7 @@ __version__ = '0.2'
 tutum.user_agent = "tutum-haproxy/%s" % __version__
 
 DEBUG = os.getenv("DEBUG", False)
+PIDFILE = "/tmp/tutum-haproxy.pid"
 
 logger = logging.getLogger("haproxy")
 
@@ -51,19 +53,36 @@ def tutum_event_handler(event):
             run_haproxy(msg)
 
 
+def create_pid_file():
+    pid = str(os.getpid())
+    try:
+        file(PIDFILE, 'w').write(pid)
+    except Exception as e:
+        logger.error("Cannot write to pidfile: %s" % e)
+    return pid
+
+
+def user_reload_haproxy(signum, frame):
+    run_haproxy("User reload")
+
+
 def main():
     logging.basicConfig(stream=sys.stdout)
     logging.getLogger("haproxy").setLevel(logging.DEBUG if DEBUG else logging.INFO)
 
+    pid = create_pid_file()
+    signal.signal(signal.SIGUSR1, user_reload_haproxy)
+
     if Haproxy.cls_container_uri and Haproxy.cls_service_uri:
         if Haproxy.cls_tutum_auth:
-            logger.info("HAProxy has access to Tutum API - will reload list of backends in real-time")
+            logger.info(
+                "Tutum-haproxy(PID: %s) has access to Tutum API - will reload list of backends in real-time" % pid)
         else:
             logger.warning(
-                "HAProxy doesn't have access to Tutum API and it's running in Tutum - you might want to give "
-                "an API role to this service for automatic backend reconfiguration")
+                "Tutum-haproxy(PID: %s) doesn't have access to Tutum API and it's running in Tutum - you might want to"
+                " give an API role to this service for automatic backend reconfiguration" % pid)
     else:
-        logger.info("HAProxy is not running in Tutum")
+        logger.info("Tutum-haproxy(PID: %s) is not running in Tutum" % pid)
 
     if Haproxy.cls_container_uri and Haproxy.cls_service_uri and Haproxy.cls_tutum_auth:
         events = tutum.TutumEvents()
